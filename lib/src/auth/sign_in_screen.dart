269c3497b9675a2ba790a2b83acd7223f1efd347
h2danilo <h2danilo@hotmail.com>
2022-10-26 13:54:34 -0300

    Tela Home - Add AppBar, Badge e Campo Pesquisa

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:quitanda/src/auth/sign_up_screen.dart';
+import 'package:quitanda/src/base/base_screen.dart';
 
 import '../config/custom_colors.dart';
 import 'components/custom_text_field.dart';
@@ -102,7 +103,13 @@ class SignInScreen extends StatelessWidget {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(18)),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (c) {
+                            return const BaseScreen();
+                          }),
+                        );
+                      },
                       child: const Text(
                         "Entrar",
                         style: TextStyle(fontSize: 18),
---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,104 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:quitanda/src/config/custom_colors.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // App bar
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+            children: [
+              TextSpan(
+                text: "Valim",
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+              TextSpan(
+                text: "Quitanda",
+                style: TextStyle(
+                  color: CustomColors.customConstrastColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customConstrastColor,
+                badgeContent: const Text(
+                  "2",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+
+      body: Column(
+        children: [
+          // Campo de Pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: "Pesquise aqui ...",
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customConstrastColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          )
+          //Categorias
+
+          //Grid
+        ],
+      ),
+    );
+  }
+}
